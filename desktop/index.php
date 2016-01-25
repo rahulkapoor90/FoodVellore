7fdac5d725cb3f0d6dc13c821f7bcd792f22f756
Rahul Kapoor <rahulkapoorbbps@outlook.com>
2016-01-26 02:58:31 +0530

    Register complete.

--- desktop/index.php
+++ desktop/index.php
@@ -1,8 +1,13 @@
 <!DOCTYPDE html>
-<html>
+<html lang="en">
 <head>
 <link rel="stylesheet" href="materialize/css/materialize.min.css">
 <title>FoodVellore</title>
+<link rel="stylesheet" href="stylesheets/style.css" type="text/css"/>
+ <meta charset="utf-8"/>
+ <meta content="noodp,noydir" name="robots"/>
+ <meta content="FoodVellore is a great platform for simplicity and Transparency." name="description"/>
+<link type="image/x-icon" rel="shortcut icon" href="images/favicon.ico"/>
   <!-- Compiled and minified JavaScript -->
   <script src="materialize/js/materialize.min.js"></script>
   <script src="scripts/script.js"></script>
@@ -10,13 +15,11 @@
 <body>
   <nav>
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Logo</a>
+      <a href="#!" class="brand-logo">FoodVellore</a>
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
       </ul>
       <ul class="side-nav" id="mobile-demo">
         <li><a href="sass.html">Sass</a></li>
