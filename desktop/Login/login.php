bad633baa7047c7c3125ea4f4e4ccfed708e90f0
gopalchdk <gopalchdk@rediffmail.com>
2016-01-26 02:36:55 +0530

    login

--- desktop/Login/login.php
+++ desktop/Login/login.php
@@ -6,7 +6,30 @@
 	  <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="./materialize/css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="../materialize/css/materialize.min.css"  media="screen,projection"/>
       <!--Let browser know website is optimized for mobile-->
+      <link type="text/css" rel="stylesheet" href="../css/login.css">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
+<body>
+	<div class="container">
+		<div class="col s12 m12 l4 offset-l4 z-depth-5 container login_box">
+			<form name="login" action="auth.php" method="post" class="container">
+				<br />
+				<h4>Username</h4>
+				<input type="text" name="username" placeholder="Enter the Username" required />
+				<h4>Password</h4>
+				<input type="password" name="password" placeholder="Enter your password" required />
+				<br />
+				<div class="row">
+					<div class="col l4 offset-l4 s12">
+				      <input type="submit" class="btn waves-effect waves-light z-depth-2 login_submit" value="LogIn" name="login_submit">
+				    </div>
+				</div>
+			</form>
+		</div>
+	</div>
+	<script type="text/javascript" src="../materialize/js/materialize.min.js" />
+</body>
+</html>
+
