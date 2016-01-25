eb01088c39aa9b1b60e366ef7ae994a336bd7b29
Rahul Kapoor <rahulkapoorbbps@outlook.com>
2016-01-26 01:42:20 +0530

    Minor changes.

--- desktop/index.php
+++ desktop/index.php
@@ -1,1 +1,32 @@
-////
+<!DOCTYPDE html>
+<html>
+<head>
+<link rel="stylesheet" href="materialize/css/materialize.min.css">
+<title>FoodVellore</title>
+  <!-- Compiled and minified JavaScript -->
+  <script src="materialize/js/materialize.min.js"></script>
+  <script src="scripts/script.js"></script>
+</head>
+<body>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo">Logo</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">Javascript</a></li>
+        <li><a href="mobile.html">Mobile</a></li>
+      </ul>
+    </div>
+  </nav>
+          
+        
+</body>
+</html>
