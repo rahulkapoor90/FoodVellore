573ba14a107beb79b76d3e9a798c02349bca2b3d
Sulabh Agarwal <sulabhrocks155@gmail.com>
2016-01-27 13:58:03 +0530

    One Menu Icon only

--- desktop/index.php
+++ desktop/index.php
@@ -19,7 +19,7 @@
   <nav>
     <div class="nav-wrapper">
       <a href="#!" class="brand-logo">FoodVellore</a>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+  <!--    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a> -->
   <ul class="right hide-on-med-and-down">
     <li><a href="#!">Home</a></li>
     <li><a href="#!">Contact Us</a></li>
