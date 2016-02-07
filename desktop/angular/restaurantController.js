99eb69f6667f512202bdbebc400fa5848a77870e
gopalchdk <gopalchdk@rediffmail.com>
2016-02-07 15:59:35 +0530

    profile
    menu display

---
+++ desktop/angular/restaurantController.js
@@ -0,0 +1,9 @@
+myApp.controller('restaurantController', function($scope, $http){
+    $http.get('apna.json').success(function (data){
+        $scope.items = data;
+    });
+
+    $scope.process = function(name,value){
+    	$scope.total = parseInt($scope.total) + parseInt(value);
+    }
+});
---
+++ desktop/restaurants/index.php
@@ -0,0 +1,40 @@
+<?php
+$_SESSION['restaurant'] = "apna_dhaba";
+?>
+<html lang="en-us">
+<head>
+  <meta charset="utf-8">
+<link rel="stylesheet" href="../materialize/css/materialize.min.css">
+ <link type="image/x-icon" rel="shortcut icon" href="http://i.imgur.com/xl1WjhW.png"/>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<title>FoodONZ | Order Food in Vellore</title>
+ <meta charset="utf-8"/>
+ <meta content="FoodVellore is a great platform for simplicity and Transparency." name="description"/>
+<link type="image/x-icon" rel="shortcut icon" href="images/favicon.ico"/>
+  <!-- Compiled and minified JavaScript -->
+ 
+   <!-- Site's designed for mobile -->
+ <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/> 
+</head>
+<body ng-app="myApp">
+	<div ng-controller="restaurantController" ng-init="total=0" clas="container">
+		<div class="col l4 s4 container">
+		<label>Search</label>    <input type="text" ng-model="search.name">
+	</div>
+		<div ng-repeat="item in items | filter:search" class="container">
+			<h3>{{item.name}}</h3>
+			<p>category:{{item.category}}</p>
+			<p>price:INR {{item.price}} /-</p>
+			<br/>
+			<button ng-click="process(item.name,item.price)">Add</button>
+		</div>
+		<h1>Total:<span ng-model="total">{{total}}</span></h1>
+	</div>
+	<script src="../angular/app.js"></script>
+	<script src="../angular/restaurantController.js"></script>
+	<script src="../materialize/js/materialize.min.js"></script>
+</body>
+</html>
