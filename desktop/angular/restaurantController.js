0c0bbe85a712d73ef7ace958f68d10de9c21d3be
gopalchdk <gopalchdk@rediffmail.com>
2016-02-28 23:21:58 +0530

    json view updated

--- desktop/angular/restaurantController.js
+++ desktop/angular/restaurantController.js
@@ -7,10 +7,10 @@ myApp.controller('restaurantController', ['$scope', '$http', function ($scope, $
     $scope.orders = [];
     $scope.norder = false;
     $scope.process = function (item) {
-        $scope.norder = true;
         if (isNaN(item.quantity)) {
             alert("please enter the quantity");
         } else {
+            $scope.norder = true;
             $scope.total = parseInt($scope.total) + item.quantity * parseInt(item.price);
             item.added = true;
             var curItem = {};
@@ -23,6 +23,9 @@ myApp.controller('restaurantController', ['$scope', '$http', function ($scope, $
     $scope.rprocess = function (item) {
         $scope.total = parseInt($scope.total) - item.quantity * parseInt(item.price);
         item.added = false;
+        if ($scope.total == 0) {
+            $scope.norder = false;
+        }
         for (var i = 0; i < $scope.orders.length; i++) {
             if (($scope.orders[i].name).match(item.name)) {
                 ($scope.orders).splice(i, 1);
--- desktop/restaurants/index.php
+++ desktop/restaurants/index.php
@@ -7,10 +7,15 @@ $_SESSION['rname'] = $res.'.json';
     <head>
         <meta charset="utf-8">
         <link type="image/x-icon" rel="shortcut icon" href="http://i.imgur.com/xl1WjhW.png" />
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script src="../angular/angular.min.js"></script>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <title>FoodONZ | Order Food in Vellore</title>
         <meta charset="utf-8" />
         <meta content="FoodVellore is a great platform for simplicity and Transparency." name="description" />
@@ -18,29 +23,44 @@ $_SESSION['rname'] = $res.'.json';
         <!-- Compiled and minified JavaScript -->
 
         <!-- Site's designed for mobile -->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
     </head>
 
     <body ng-app="myApp" ng-init="norder=false;total=0;something='<?php echo $_SESSION['rname'] ?>'">
         <div ng-controller="restaurantController" class="container">
-            <div class="col l4 s4 container">
+            <div class="col-lg-12 col-sm-12 container">
                 <label>Search</label>
                 <input type="text" ng-model="search.name">
             </div>
-            <div class="container">
-                <li ng-repeat="user in items">
-                    <p>{{user.type}}</p>
-                    <ul ng-repeat="item in user.options">
-                        <li>
-                            <p>{{item.name}}</p>
-                            <p>{{item.price}}</p>
-                            <br/>
-                            <input type="number" ng-hide="item.added" placeholder="enter quantity" ng-model="item.quantity" required>
-                            <button ng-hide="item.added" ng-click="process(item)">Add</button>
-                            <button ng-show="item.added" ng-click="rprocess(item)" class="ng-cloak">Remove</button>
-                        </li>
-                    </ul>
-                </li>
+            <br />
+            <div class="panel-group" id="accordion">
+                <div ng-repeat="user in items" class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#{{user.type}}">
+        {{user.type}}</a>
+      </h4>
+                    </div>
+                    <div id="{{user.type}}" class="panel-collapse collapse in">
+                        <div ng-repeat="item in user.options">
+                            <div class="row">
+                                <div class="col-sm-12 col-lg-3">
+                                    <p>{{item.name}}</p>
+                                </div>
+                                <div class="col-sm-12 col-lg-3">
+                                    <p>{{item.price}}</p>
+                                </div>
+                                <div class="col-sm-12 col-lg-3">
+                                    <input type="number" ng-hide="item.added" placeholder="enter quantity" ng-model="item.quantity" required>
+                                </div>
+                                <div class="col-sm-12 col-lg-3">
+                                    <button ng-hide="item.added" ng-click="process(item)">Add</button>
+                                    <button ng-show="item.added" ng-click="rprocess(item)" class="ng-cloak">Remove</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <h1>Total:<span ng-model="total">{{total}}</span></h1>
             <button data-target="modal1" class="btn modal-trigger">Your Orders</button>
@@ -56,7 +76,7 @@ $_SESSION['rname'] = $res.'.json';
                     <h3 ng-show="norder">Bill Amount:{{total}}</h3>
                 </div>
             </div>
-            <button class="btn waves-light" ng-show="norder" ng-click="checkout()">Book</button>
+            <button class="btn btn-primary" ng-show="norder" ng-click="checkout()">Book</button>
         </div>
         <script src="../angular/app.js"></script>
         <script src="../angular/restaurantController.js"></script>
