7891840ee9ceb14d923fc5ad30c900ea96e1a182
gopalchdk <gopalchdk@rediffmail.com>
2016-02-28 18:18:09 +0530

    json changes

--- desktop/angular/restaurantController.js
+++ desktop/angular/restaurantController.js
@@ -1,46 +1,47 @@
-myApp.controller('restaurantController',['$scope','$http', function($scope, $http){
-    $http.get($scope.something).success(function (data){
+myApp.controller('restaurantController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('apna.json').success(function (data) {
         $scope.items = data;
+    }).error(function (data) {
+        console.log(data);
     });
     $scope.orders = [];
-    $scope.norder=false;
-    $scope.process = function(item){
+    $scope.norder = false;
+    $scope.process = function (item) {
         $scope.norder = true;
-        if(isNaN(item.quantity)){
+        if (isNaN(item.quantity)) {
             alert("please enter the quantity");
+        } else {
+            $scope.total = parseInt($scope.total) + item.quantity * parseInt(item.price);
+            item.added = true;
+            var curItem = {};
+            curItem.name = item.name;
+            curItem.price = item.quantity * parseInt(item.price);
+            curItem.quantity = item.quantity;
+            $scope.orders.push(curItem);
         }
-        else{
-    	$scope.total = parseInt($scope.total) + item.quantity*parseInt(item.price);
-    	item.added=true;
-        var curItem  = {};
-        curItem.name = item.name;
-        curItem.price = item.quantity*parseInt(item.price);
-        curItem.quantity = item.quantity;
-        $scope.orders.push(curItem);
     }
-    }
-    $scope.rprocess = function(item){
-    	$scope.total = parseInt($scope.total) - item.quantity*parseInt(item.price);
-    	item.added=false;
-        for(var i=0;i<$scope.orders.length;i++){
-            if(($scope.orders[i].name).match(item.name)){
-                ($scope.orders).splice(i,1);
+    $scope.rprocess = function (item) {
+        $scope.total = parseInt($scope.total) - item.quantity * parseInt(item.price);
+        item.added = false;
+        for (var i = 0; i < $scope.orders.length; i++) {
+            if (($scope.orders[i].name).match(item.name)) {
+                ($scope.orders).splice(i, 1);
             }
         }
     }
-    $scope.checkout = function(){
-         $http({
-         url:"../restaurants/middleware.php",
-         method : 'POST',
-         data :{
-            total : JSON.stringify($scope.orders),
-            amount : $scope.total
-        }
-    }).success(function(data, status, headers, config) {
-        console.log(data);
-        window.location.href = '../restaurants/checkout.php';
-    }).error(function(data, status, headers, config) {
-       console.log("not done");
-});
+    $scope.checkout = function () {
+        $http({
+            url: "../restaurants/middleware.php",
+            method: 'POST',
+            data: {
+                total: JSON.stringify($scope.orders),
+                amount: $scope.total
+            }
+        }).success(function (data, status, headers, config) {
+            console.log(data);
+            window.location.href = '../restaurants/checkout.php';
+        }).error(function (data, status, headers, config) {
+            console.log("not done");
+        });
     }
 }]);
--- desktop/restaurants/index.php
+++ desktop/restaurants/index.php
@@ -2,62 +2,67 @@
 $res= "apna";
 $_SESSION['rname'] = $res.'.json';
 ?>
-<html lang="en-us">
-<head>
-  <meta charset="utf-8">
-<link rel="stylesheet" href="../materialize/css/materialize.min.css">
- <link type="image/x-icon" rel="shortcut icon" href="http://i.imgur.com/xl1WjhW.png"/>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<script src="../angular/angular.min.js"></script>
+    <html lang="en-us">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<title>FoodONZ | Order Food in Vellore</title>
- <meta charset="utf-8"/>
- <meta content="FoodVellore is a great platform for simplicity and Transparency." name="description"/>
-<link type="image/x-icon" rel="shortcut icon" href="../images/favicon.ico"/>
-  <!-- Compiled and minified JavaScript -->
- 
-   <!-- Site's designed for mobile -->
- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/> 
-</head>
-<body ng-app="myApp" ng-init="norder=false;total=0;something='<?php echo $_SESSION['rname'] ?>'">
-	<div ng-controller="restaurantController"  class="container">
-		<div class="col l4 s4 container">
-		<label>Search</label>    <input type="text" ng-model="search.name">
-	</div>
-		<div ng-repeat="item in items | filter:search" class="container">
-			<h3>{{item.name}}</h3>
-			<p>category:{{item.category}}</p>
-			<p>price:INR {{item.price}} /-</p>
-			<br/>
-			<input type="number" ng-hide="item.added" placeholder="enter quantity" ng-model="item.quantity" required>
-			<button ng-hide="item.added"  ng-click="process(item)">Add</button>
-			<button ng-show="item.added" ng-click="rprocess(item)" class="ng-cloak">Remove</button>
-		</div>
-		<h1>Total:<span ng-model="total">{{total}}</span></h1>
-		<button data-target="modal1" class="btn modal-trigger">Your Orders</button>
-		<div id="modal1" class="modal bottom-sheet">
-        <div class="modal-content">
-        	<h1 ng-show="norder">Your have ordered</h1>
-        	<p ng-hide="norder">You have not ordered anything</p>
-		<div class="container" ng-repeat="order in orders">
-			<h3>{{order.name}}</h3>
-			<p>Price:{{order.price}}</p>
-			<p>Quantity:{{order.quantity}}</p>
-		</div>
-		<h3 ng-show="norder">Bill Amount:{{total}}</h3>
-	</div>
-</div>
-        <button class="btn waves-light" ng-show="norder" ng-click="checkout()">Book</button>
-	</div>
-	<script src="../angular/app.js"></script>
-	<script src="../angular/restaurantController.js"></script>
-	<script src="../materialize/js/materialize.min.js"></script>
-	<script>
-	$(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
-	</script>
-</body>
-</html>
+    <head>
+        <meta charset="utf-8">
+        <link type="image/x-icon" rel="shortcut icon" href="http://i.imgur.com/xl1WjhW.png" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="../angular/angular.min.js"></script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <title>FoodONZ | Order Food in Vellore</title>
+        <meta charset="utf-8" />
+        <meta content="FoodVellore is a great platform for simplicity and Transparency." name="description" />
+        <link type="image/x-icon" rel="shortcut icon" href="../images/favicon.ico" />
+        <!-- Compiled and minified JavaScript -->
+
+        <!-- Site's designed for mobile -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </head>
+
+    <body ng-app="myApp" ng-init="norder=false;total=0;something='<?php echo $_SESSION['rname'] ?>'">
+        <div ng-controller="restaurantController" class="container">
+            <div class="col l4 s4 container">
+                <label>Search</label>
+                <input type="text" ng-model="search.name">
+            </div>
+            <div class="container">
+                <li ng-repeat="user in items">
+                    <p>{{user.type}}</p>
+                    <ul ng-repeat="option in user.options">
+                        <li ng-repeat="item in option">
+                            <p>{{item.name}}</p>
+                            <p>{{item.price}}</p>
+
+
+                            <br/>
+                            <input type="number" ng-hide="item.added" placeholder="enter quantity" ng-model="item.quantity" required>
+                            <button ng-hide="item.added" ng-click="process(item)">Add</button>
+                            <button ng-show="item.added" ng-click="rprocess(item)" class="ng-cloak">Remove</button>
+                        </li>
+                    </ul>
+                </li>
+            </div>
+            <h1>Total:<span ng-model="total">{{total}}</span></h1>
+            <button data-target="modal1" class="btn modal-trigger">Your Orders</button>
+            <div id="modal1" class="modal bottom-sheet">
+                <div class="modal-content">
+                    <h1 ng-show="norder">Your have ordered</h1>
+                    <p ng-hide="norder">You have not ordered anything</p>
+                    <div class="container" ng-repeat="order in orders">
+                        <h3>{{order.name}}</h3>
+                        <p>Price:{{order.price}}</p>
+                        <p>Quantity:{{order.quantity}}</p>
+                    </div>
+                    <h3 ng-show="norder">Bill Amount:{{total}}</h3>
+                </div>
+            </div>
+            <button class="btn waves-light" ng-show="norder" ng-click="checkout()">Book</button>
+        </div>
+        <script src="../angular/app.js"></script>
+        <script src="../angular/restaurantController.js"></script>
+
+    </body>
+
+    </html>
